bb92bbd2189346eeb0812f347b9d21082e4e5b84
portnoyv <45538542+portnoyv@users.noreply.github.com>
2019-06-02 12:56:12 +0300

    Update pretrained_example.py

--- pretrained_example.py
+++ pretrained_example.py
@@ -28,20 +28,21 @@ def main():
         # Gs = Long-term average of the generator. Yields higher-quality results than the instantaneous snapshot.
 
     # Print network details.
-    Gs.print_layers()
+    #Gs.print_layers()
 
     # Pick latent vector.
-    rnd = np.random.RandomState(5)
-    latents = rnd.randn(1, Gs.input_shape[1])
+    n=1000
+    for iter in range(n):
+        rnd = np.random.RandomState(iter)
+        latents = rnd.randn(1, Gs.input_shape[1])
+        # Generate image.
+        fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+        images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
 
-    # Generate image.
-    fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
-    images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
-
-    # Save image.
-    os.makedirs(config.result_dir, exist_ok=True)
-    png_filename = os.path.join(config.result_dir, 'example.png')
-    PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
+        # Save image.
+        os.makedirs(config.result_dir, exist_ok=True)
+        png_filename = os.path.join(config.result_dir, str(iter) + 'example.jpg')
+        PIL.Image.fromarray(images[0], 'RGB').save(png_filename)
 
 if __name__ == "__main__":
     main()
